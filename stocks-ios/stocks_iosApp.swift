8930f948251ff5f0cd32d2afec822a17b5dec2e1
Parag Jadhav <parag2804@gmail.com>
2024-04-03 19:15:54 -0700

    Initial Commit

---
+++ stocks-ios/stocks_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  stocks_iosApp.swift
+//  stocks-ios
+//
+//  Created by Parag Jadhav on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct stocks_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
